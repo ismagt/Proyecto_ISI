5fbb087d39efee7bf444336ad82310193fdccaf4
ismagt <isma98.igm@gmail.com>
2019-10-29 16:04:01 +0100

    C2: implementación de la tabla Equipos_Tienen_Ciudades que guarda la relación entre las dos entidades

--- mysql.sql
+++ mysql.sql
@@ -1,2 +1,6 @@
 
-
+CREATE TABLE Equipos_Tienen_Ciudades 
+( id_equipo INT, id_ciudad INT
+PRIMARY KEY(id_equipo, id_ciudad)
+FOREIGN KEY (id_equipo) REFERENCES Equipos(id_equipo),
+FOREIGN KEY (id_ciudad) REFERENCES Ciudades(id_ciudad));
